b0fd2331d16c4f6021f6c01beb05a1294788a768
brissporras26 <136277527+brissporras26@users.noreply.github.com>
2023-09-30 17:19:13 -0600

    add libraries and created socket

--- README.md
+++ README.md
@@ -3,3 +3,7 @@ Final proyect for software engineer, distributed systems and web programming.
 Things done: 
 
 -install node.js
+- use npm run start to run the app 
+- use npm install to install package.json dependencies
+- add Tailwind CSS and web socket libraries 
+- tailwind css classes  https://tailwindcss.com/docs/editor-setup
--- chat/src/App.js
+++ chat/src/App.js
@@ -1,7 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import io from "socket.io-client";
+import React from "react";
 
 function App() {
+  let socket;
+  const NODE_API_URL = "localhost:3002/";
+  //For connection scoket.io  
+  React.useEffect(() => {    
+    socket = io(NODE_API_URL);  
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
